971c30b052f8766937507039dd26199c95c4ef09
Erick Benjamin Nuñez Torres <erickbeny23@gmail.com>
2022-06-16 22:49:53 -0500

    AVISOS
    SIMPLE, SENCILLO Y ERROR

---
+++ PRIMER SCRIPT/Assets/scripts/helloworld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("igual que en c, solo da un aviso");
+        Debug.Log("buzo caperuzo");
+        Debug.LogWarning("hey! pon atención");
+        Debug.LogError("Ya huele a quemado");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
